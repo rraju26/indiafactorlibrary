a2e18429316e3183760508a932e2a8c47d983c65
rraju26 <rajanraju@me.com>
2025-05-15 10:48:36 +0800

    Default headers

--- indiafactorlibrary/indiafactorlibrary.py
+++ indiafactorlibrary/indiafactorlibrary.py
@@ -44,12 +44,12 @@ class IndiaFactorLibrary:
     
     def _get_response(self, url, params=None, headers=None):
         retries = 3
+        library_version = 'v0.0.10'
         default_headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "User-Agent": f"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Library/{library_version}",
         "Accept": "application/json, text/csv"  #  acceptable content types
         }
-        if headers:
-            default_headers.update(headers)
+        
         for attempt in range(retries):
             try:
                 response = self.session.get(url, params=params, headers=default_headers, timeout=5)
